0563eb283f76bc77945f1aeedc37e944ab5d39e6
mxweng <80088258+mxweng@users.noreply.github.com>
2023-11-10 17:23:04 +0800

    update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='mxwpy',  # 包名
-    version='0.1.0',  # 版本
-    author='Your Name',  # 作者名
-    author_email='your.email@example.com',  # 作者邮箱
-    description='A short description of your package',  # 简短描述
+    version='0.1.1',  # 版本
+    author='Mingxing Weng',  # 作者名
+    author_email='2431141461@qq.com',  # 作者邮箱
+    description='efficient numerical schemes',  # 简短描述
     long_description=open('README.md').read(),  # 长描述，通常是你的README
     long_description_content_type='text/markdown',  # 长描述的格式，如果是Markdown，就是'text/markdown'
-    url='http://github.com/yourusername/your-package-name',  # 项目的主页
+    url='https://github.com/mxweng/mxwpy',  # 项目的主页
     packages=find_packages(),  # 自动发现所有包
     classifiers=[
         'Development Status :: 3 - Alpha',
