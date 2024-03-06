ee59f8af97f641a789c9449a2c0fc0a513f4b0be
mxweng <80088258+mxweng@users.noreply.github.com>
2024-03-06 13:55:40 +0800

    version update to 0.2.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mxwpy',  # package name
-    version='0.2.3',  # version
+    version='0.2.4',  # version
     author='Mingxing Weng',  # author name
     author_email='2431141461@qq.com',  # author email
     description='efficient numerical schemes',  # short description
